c9d0ef1927ecf0799d614dc65ce7e2d86bc39d9a
Spencer Martin <sm5ve@virginia.edu>
2025-03-25 16:21:52 +0100

    Wrap architecture-specific behavior in panic.cpp in #ifdef

--- kernel/lib/panic.cpp
+++ kernel/lib/panic.cpp
@@ -9,9 +9,11 @@ namespace kernel{
     void panic(const char* message, const char* filename, const uint32_t line){
         kernel::DbgOut << "Panic: " << message << "\n";
         kernel::DbgOut << "In file " << filename << " line " << line << "\n";
+#ifdef __x86_64__
         asm volatile("outw %0, %1" ::"a"((uint16_t)0x2000), "Nd"((uint16_t)0x604)); //Quit qemu
         for(;;)
             asm volatile("hlt");
+#endif
     }
 }
 
