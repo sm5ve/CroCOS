7a6c8579eef135fd5282a9795028a774ac876fb6
Spencer Martin <sm5ve@virginia.edu>
2026-06-01 11:12:50 -0700

    Extend sampling time for stress test

--- kernel/KernelMain.cpp
+++ kernel/KernelMain.cpp
@@ -71,7 +71,7 @@ namespace kernel{
         timing::enqueueEvent([] {
             klog() << "\nGoodbye :)\n";
             asm volatile("outw %0, %1" ::"a"((uint16_t)0x2000), "Nd"((uint16_t)0x604));
-        }, 2000);
+        }, 20000);
         return true;
     }
 
